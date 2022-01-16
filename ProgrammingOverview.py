175793d0b7a0a528b5a2a6262464f70273479fc8
Waffl3Development <70445191+Waffl3Development@users.noreply.github.com>
2022-01-16 18:10:40 -0500

    dd

--- ProgrammingOverview.py
+++ ProgrammingOverview.py
@@ -7,7 +7,7 @@ if x < 0:
     print(x)
 
 
-Compare = 10
+Compare = 10 #variable #integer
 hello(Compare)#hello (10)
 
 def hello(just): #hello(10)
@@ -21,32 +21,41 @@ def hello(just): #hello(10)
 
 
 HOUSE = 5 + 7 #12
+print("5" + 7) #"57"
+
 print(HOUSE)
 
-print("5" + 7) #57
+
 
 something = "string"
 
 5 + 5 #int 1
 5 - 5 #int 1
 5 * 5 #int 1
-5 / 5 #float 1.0
+5 / 5 #float 1.0 8/3 2.666
+
 5 // 5 #int 1 floor down to nearest whole number
+8 // 3 #2
+
+
+#arrays{}
+variable = [1, 4, 5, 6] #global used in a lot of other languages
+          #[0, 1, 2, 3]
+variable[2] #1
 
 word = "sorry"
 word[0] #s
 word[-1] #y
-word[2:] #orry
-word[:2] #so
-
-variable = [1, 4, 5, 6] #global used in a lot of other languages
-variable[0] #1
+word[1:] #orry
+word[:1] #so
+#}
 
 # while x < 0 statements
 u = 0
 while u < 10:
     u = u + 1 #u++
             #Most often used ++u
+#variable = [1, 4, 5, 6]
 for i in variable: #i is a number
     print(i)    #1
                 #4
@@ -61,13 +70,12 @@ for i in range(5): #i is 0
 #4
 
 u=0
-r=0
 while u < 5:#0
+    r=0
     while r < 10:#10
         print(u,r)
         #0,0
         #0,1
         #0,2
-
         r = r + 1
     u = u + 1
--- pythontest.py
+++ pythontest.py
@@ -1,8 +1,10 @@
-u=0
+#u=0
+#import os
+#while (u < 5):#0
+#    r=0
+#    while (r < 10):#10
+#        print(u,r)
+#        r = r + 1
+#    u = u + 1
 
-while (u < 5):#0
-    r=0
-    while (r < 10):#10
-        print(u,r)
-        r = r + 1
-    u = u + 1
+#print("im a little teapot, "+ os.getlogin())
